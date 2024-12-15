97faf80a89e719905b5a5e0d5925642ccc235519
Agnivom <agnivmitra11@gmail.com>
2024-12-15 19:10:52 +0530

    Create TreeNodeStructure.c

---
+++ Graphs/TreeNodeStructure.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int data) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
