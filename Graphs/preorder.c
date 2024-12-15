c21e08c247196b7f1255f0096fbb03b2e74d7e5f
Agnivom <agnivmitra11@gmail.com>
2024-12-15 19:14:04 +0530

    Create preorder.c

---
+++ Graphs/preorder.c
@@ -0,0 +1,17 @@
+#include <stack>
+
+void preorderNonRecursive(struct TreeNode* root) {
+    if (root == NULL) return;
+    std::stack<struct TreeNode*> stack;
+    stack.push(root);
+
+    while (!stack.empty()) {
+        struct TreeNode* current = stack.top();
+        stack.pop();
+        printf("%d ", current->data);
+        
+        // Push right and left children to stack (right first)
+        if (current->right != NULL) stack.push(current->right);
+        if (current->left != NULL) stack.push(current->left);
+    }
+}
