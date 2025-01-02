50b0b00515d4330f4dc1182364822fb3e58c0a29
Agnivom <agnivmitra11@gmail.com>
2025-01-02 20:20:04 +0530

    Update QueueUsingArray.c

--- Stack_Queue/QueueUsingArray.c
+++ Stack_Queue/QueueUsingArray.c
@@ -11,17 +11,27 @@ struct Queue {
 // Function to initialize the queue
 void initQueue(struct Queue* queue) {
     queue->front = -1;
-    queue->rear = -1;
+    queue->rear = -1;   //initializes both at -1
 }
 
 // Function to check if the queue is full
 int isFull(struct Queue* queue) {
-    return queue->rear == MAX - 1;
+    if (queue->front==queue->size-1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 // Function to check if the queue is empty
 int isEmpty(struct Queue* queue) {
-    return queue->front == -1 || queue->front > queue->rear;
+    if (queue->rear==queue->rear){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 // Function to enqueue an element
@@ -32,23 +42,29 @@ void enqueue(struct Queue* queue, int value) {
         if (queue->front == -1) {
             queue->front = 0;  // If the queue is empty, set front to 0
         }
-        queue->arr[++queue->rear] = value;
+        queue->rear++;  //updates rear pointer 
+        queue->arr[queue->rear]=value;  //inserts value
         printf("%d enqueued\n", value);
     }
 }
 
 // Function to dequeue an element
 int dequeue(struct Queue* queue) {
-    if (isEmpty(queue)) {
-        printf("Queue Underflow\n");
-        return -1;  // Error condition
-    } else {
-        return queue->arr[queue->front++];  // Return the front element and increment front
+    int a=-1;
+    if isFull(queue){
+        printf ("Queue is empty");
     }
+    else{
+        queue->front++;
+        a= queue->arr[queue->front];
+    }
+    return a;
 }
 
 int main() {
     struct Queue queue;
+    queue.arr=(int*)malloc(queue.size*sizeof(int));
+    queue.size=100;
     initQueue(&queue);
 
     enqueue(&queue, 10);
